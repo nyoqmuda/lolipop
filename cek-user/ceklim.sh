b17d1590865d3ddffec03bd96c7ffe3c28fe9b97
nyoqmuda <123232999+nyoqmuda@users.noreply.github.com>
2023-02-16 01:31:46 +0800

    Add files via upload

---
+++ cek-user/ceklim.sh
@@ -0,0 +1,23 @@
+#wget https://github.com/${GitUser}/
+GitUser="nyoqmuda"
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+
+echo " "
+echo "===========================================";
+echo " ";
+if [ -e "/root/log-limit.txt" ]; then
+echo "User Who Violate The Maximum Limit";
+echo "Time - Username - Number of Multilogin"
+echo "-------------------------------------";
+cat /root/log-limit.txt
+else
+echo " No user has committed a violation"
+echo " "
+echo " or"
+echo " "
+echo " The user-limit script not been executed."
+fi
+echo " ";
+echo "===========================================";
+echo " ";
