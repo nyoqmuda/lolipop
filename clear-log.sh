2c7a2596fdf2e664ec4c3f0c0cee51d06ae03448
nyoqmuda <123232999+nyoqmuda@users.noreply.github.com>
2023-02-16 01:39:39 +0800

    Add files via upload

---
+++ clear-log.sh
@@ -0,0 +1,28 @@
+#wget https://github.com/${GitUser}/
+GitUser="nyoqmuda"
+#IZIN SCRIPT
+MYIP=$(curl -sS ipv4.icanhazip.com)
+
+data=(`find /var/log/ -name *.log`);
+for log in "${data[@]}"
+do
+echo "$log clear"
+echo > $log
+done
+data=(`find /var/log/ -name *.err`);
+for log in "${data[@]}"
+do
+echo "$log clear"
+echo > $log
+done
+data=(`find /var/log/ -name mail.*`);
+for log in "${data[@]}"
+do
+echo "$log clear"
+echo > $log
+done
+echo > /var/log/syslog
+echo > /var/log/btmp
+echo > /var/log/messages
+echo > /var/log/debug
+echo -e "Script By kurita"
---
+++ xp.sh
@@ -0,0 +1,111 @@
+#wget https://github.com/${GitUser}/
+GitUser="nyoqmuda"
+
+# // IZIN SCRIPT
+export MYIP=$(curl -sS ipv4.icanhazip.com)
+
+# // XRAY VMESS WS
+data=( `cat /usr/local/etc/xray/vmess.json | grep '^#vms' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^#vms $user" "/usr/local/etc/xray/vmess.json" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+
+sed -i "/^#vms $user $exp/,/^},{/d" /usr/local/etc/xray/vmess.json
+sed -i "/^#vms $user $exp/,/^},{/d" /usr/local/etc/xray/vmessnone.json
+
+systemctl disable xray@$user-tls.service
+systemctl stop xray@$user-tls.service
+systemctl disable xray@$user-none.service
+systemctl stop xray@$user-none.service
+
+rm -f "/usr/local/etc/xray/$user-tls.json"
+rm -f "/usr/local/etc/xray/$user-none.json"
+rm -f "/usr/local/etc/xray/$user-clash-for-android.yaml"
+rm -f "/home/vps/public_html/$user-clash-for-android.yaml"
+fi
+done
+
+systemctl restart xray@vmess
+systemctl restart xray@vmessnone
+
+# // XRAY VLESS WS
+data=( `cat /usr/local/etc/xray/vless.json | grep '^#vls' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^#vls $user" "/usr/local/etc/xray/vless.json" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "/^#vls $user $exp/,/^},{/d" /usr/local/etc/xray/vless.json
+sed -i "/^#vls $user $exp/,/^},{/d" /usr/local/etc/xray/vlessnone.json
+fi
+done
+
+systemctl restart xray@vless
+systemctl restart xray@vlessnone
+
+# XRAY TROJAN WS TLS
+data=( `cat /usr/local/etc/xray/trojan.json | grep '^#trws' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^#trws $user" "/usr/local/etc/xray/trojan.json" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+
+sed -i "/^#trws $user $exp/,/^},{/d" /usr/local/etc/xray/trojan.json
+sed -i "/^#trws $user $exp/,/^},{/d" /usr/local/etc/xray/trojannone.json
+
+fi
+done
+
+systemctl restart xray@trojan
+systemctl restart xray@trojannone
+
+# XRAY VLESS XTLS DIRECT
+data=( `cat /usr/local/etc/xray/config.json | grep '^#vxtls' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^#vxtls $user" "/usr/local/etc/xray/config.json" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "/^#vxtls $user $exp/,/^},{/d" /usr/local/etc/xray/config.json
+fi
+done
+
+systemctl restart xray.service
+
+# // XRAY TROJAN TCP TLS
+data=( `cat /usr/local/etc/xray/akunxtr.conf | grep '^###' | cut -d ' ' -f 2`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/usr/local/etc/xray/akunxtr.conf" | cut -d ' ' -f 3)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" = "0" ]]; then
+sed -i "/^#trx $user $exp/,/^},{/d" /usr/local/etc/xray/tcp.json
+sed -i "/^### $user $exp/d" "/usr/local/etc/xray/akunxtr.conf"
+fi
+done
+
+systemctl restart xray@tcp
+
+echo -e " Delete Exp User Xray Success"
+echo 
+echo -e " Back To Menu In 2 Sec"
+sleep 2
+menu
